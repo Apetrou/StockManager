10a0171074e70ad092621945cac43a39569873a8
Alkis Petrou <petroualkis@gmail.com>
2018-07-27 11:18:15 +0100

    dependancies organized

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,12 +2,12 @@
 // require('../css/global.scss');
 const $ = require('jquery');
 require('bootstrap');
-require('typeahead.js');
+// require('typeahead.js');
 require('@fortawesome/fontawesome-free/js/all.js');
 
 $(document).ready(function() {
 
-    $('[data-toggle="popover"]').popover();
+    // $('[data-toggle="popover"]').popover();
 
     $('body').on('keypress','#search',function(){
         $('#search').siblings('span.input-group-btn').find('i.fa-refresh.fa-spin').removeClass('fa-refresh fa-spin').addClass('fa-search');
@@ -18,34 +18,34 @@ $(document).ready(function() {
     });
 
 
-    $("#search").typeahead({
-        source: function(query, process) {
-            return $.ajax({
-                type: "POST",
-                url: "{{ path('ajax_search') }}",
-                data: {
-                    'query' : $("#search").val()
-                },
-                dataType: "json",
-                success: function( data ) {
-                    console.log(data['customers']);
-                    // response( data['customers'] );
-                }
-            });
-        },
-        // source: {
-        //     data: ["Alkis","Palkis"]
-        // },
-        // minLength: 3,
-        // select: function( event, ui ) {
-        //     $("#search").val("");
-        //     if(ui.item.value == "No customer found, click to create new...") {
-        //         location.href = '/customer/new';
-        //     } else {
-        //         location.href = '/customer/' + ui.item.value;
-        //     }
-        // },
-
-    });
+    // $("#search").typeahead({
+    //     source: function(query, process) {
+    //         return $.ajax({
+    //             type: "POST",
+    //             url: "{{ path('ajax_search') }}",
+    //             data: {
+    //                 'query' : $("#search").val()
+    //             },
+    //             dataType: "json",
+    //             success: function( data ) {
+    //                 console.log(data['customers']);
+    //                 // response( data['customers'] );
+    //             }
+    //         });
+    //     },
+    //     // source: {
+    //     //     data: ["Alkis","Palkis"]
+    //     // },
+    //     // minLength: 3,
+    //     // select: function( event, ui ) {
+    //     //     $("#search").val("");
+    //     //     if(ui.item.value == "No customer found, click to create new...") {
+    //     //         location.href = '/customer/new';
+    //     //     } else {
+    //     //         location.href = '/customer/' + ui.item.value;
+    //     //     }
+    //     // },
+    //
+    // });
 });
 
